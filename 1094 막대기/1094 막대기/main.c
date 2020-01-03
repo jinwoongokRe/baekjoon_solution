e5d5feb714e3517b0c8d958545490a8a0974d8fa
cbycongbab <jinwoongoknaver@gmail.com>
2020-01-03 17:05:46 +0900

    1094 막대기

---
+++ 1094 막대기/1094 막대기/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  1094 막대기
+//
+//  Created by 장진웅 on 05/08/2019.
+//  Copyright © 2019 장진웅. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int stick=64;
+    temp
+    return 0;
+}
